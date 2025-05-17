02457c0370d6a9014057640d84142f42a6234d4a
Ryan Winchester <ryan@winchester.dev>
2025-05-17 11:01:04 -0300

    Cleaning up

--- lib/sst_secret_config_provider.ex
+++ lib/sst_secret_config_provider.ex
@@ -5,19 +5,21 @@ defmodule SSTSecretConfigProvider do
 
   ## Usage
 
-      []
+      releases: [
+        demo: [
+          config_providers: [
+            {SSTSecretConfigProvider, []}
+          ]
+        ]
+      ]
 
   ## Options
 
-   - `:sst_key` - The decryption key. If it is not provided, then the `SST_KEY`
-     environment variable is checked.
+   - `:sst_key` - The base64-encoded decryption key. If it is not provided, then
+     the `SST_KEY` environment variable is checked.
 
    - `:sst_key_file` - The file with encrypted secret data. If it is not
      provided, then the `SST_KEY_FILE` environment variable is checked.
-
-   - `:sst_key_file_data` - You can pass the encrypted data directly as a
-     base64-encoded string if you prefer. Does not check an environment
-     variable.
 
   """
 
@@ -25,19 +27,23 @@ defmodule SSTSecretConfigProvider do
 
   @impl Config.Provider
   def init(opts) do
-    Keyword.validate!(opts, [:sst_key, :sst_key_file, :sst_key_file_data])
+    Keyword.validate!(opts, [:sst_key, :sst_key_file])
 
-    key = fetch_opt(opts, :sst_key, "SST_KEY") |> Base.decode64!()
+    key =
+      opts
+      |> fetch_opt!(:sst_key, "SST_KEY")
+      |> Base.decode64!()
 
-    encrypted_data =
-      case Keyword.get(opts, :sst_key_file_data) do
-        nil -> fetch_opt(opts, :sst_key_file, "SST_KEY_FILE") |> File.read!()
-        data -> Base.decode64!(data)
-      end
+    result =
+      opts
+      |> fetch_opt!(:sst_key_file, "SST_KEY_FILE")
+      |> File.read!()
+      |> decrypt(key)
 
-    encrypted_data
-    |> decode_keyfile(key)
-    |> JSON.decode!()
+    case result do
+      :error -> raise "Could not decrypt SST secrets file"
+      data -> JSON.decode!(data)
+    end
   end
 
   @impl Config.Provider
@@ -49,20 +55,18 @@ defmodule SSTSecretConfigProvider do
   @doc """
   Decode the encrypted data that was stored in the keyfile, using the key.
   """
-  def decode_keyfile(encrypted_data, key) do
+  def decrypt(encrypted_data, key) do
     cipher_size = byte_size(encrypted_data) - 16
 
     <<ciphertext::binary-size(cipher_size), auth_tag::binary-16>> = encrypted_data
 
-    # 12 zero bytes nonce.
     iv = <<0::96>>
-    # There is no additional auth data.
     aad = <<>>
 
     :crypto.crypto_one_time_aead(:aes_256_gcm, key, iv, ciphertext, aad, auth_tag, false)
   end
 
-  defp fetch_opt(opts, key, env_key) do
+  defp fetch_opt!(opts, key, env_key) do
     with nil <- Keyword.get(opts, key),
          nil <- System.get_env(env_key) do
       raise ArgumentError, message: "#{key} option or #{env_key} env var is required"
