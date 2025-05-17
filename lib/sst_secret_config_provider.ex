2697d66a068dea7b87c5baa0ff005a0fc9f4797b
Ryan Winchester <ryan@winchester.dev>
2025-05-17 10:48:03 -0300

    Start with moduledoc and options

--- lib/sst_secret_config_provider.ex
+++ lib/sst_secret_config_provider.ex
@@ -2,17 +2,40 @@ defmodule SSTSecretConfigProvider do
   @moduledoc """
   Config Provider that loads the SST secrets from an encrypted file
   and merges them into the Config.
+
+  ## Usage
+
+      []
+
+  ## Options
+
+   - `:sst_key` - The decryption key. If it is not provided, then the `SST_KEY`
+     environment variable is checked.
+
+   - `:sst_key_file` - The file with encrypted secret data. If it is not
+     provided, then the `SST_KEY_FILE` environment variable is checked.
+
+   - `:sst_key_file_data` - You can pass the encrypted data directly as a
+     base64-encoded string if you prefer. Does not check an environment
+     variable.
+
   """
 
   @behaviour Config.Provider
 
   @impl Config.Provider
   def init(opts) do
-    key = Keyword.fetch!(opts, :key) |> Base.decode64!()
+    Keyword.validate!(opts, [:sst_key, :sst_key_file, :sst_key_file_data])
 
-    opts
-    |> Keyword.fetch!(:key_file)
-    |> File.read!()
+    key = fetch_opt(opts, :sst_key, "SST_KEY") |> Base.decode64!()
+
+    encrypted_data =
+      case Keyword.get(opts, :sst_key_file_data) do
+        nil -> fetch_opt(opts, :sst_key_file, "SST_KEY_FILE") |> File.read!()
+        data -> Base.decode64!(data)
+      end
+
+    encrypted_data
     |> decode_keyfile(key)
     |> JSON.decode!()
   end
@@ -37,5 +60,12 @@ defmodule SSTSecretConfigProvider do
     aad = <<>>
 
     :crypto.crypto_one_time_aead(:aes_256_gcm, key, iv, ciphertext, aad, auth_tag, false)
+  end
+
+  defp fetch_opt(opts, key, env_key) do
+    with nil <- Keyword.get(opts, key),
+         nil <- System.get_env(env_key) do
+      raise ArgumentError, message: "#{key} option or #{env_key} env var is required"
+    end
   end
 end
